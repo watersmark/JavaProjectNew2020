48840f144be965e724415a70e1310f687786c6be
watersmark <DuldinEV@mail.ru>
2020-09-03 19:29:05 +0300

    Exception try catch

--- JavaFirst/src/main.java
+++ JavaFirst/src/main.java
@@ -1,43 +1,32 @@
-class Test_Static{
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Calendar;
 
-    public static final int x = 5;
-    public  double y = (double) (Math.random() * 3);
+class test1{
 
-}
-
-class Test_Static_UnStatic{
-
-    public static int xor1 = 32;
-    public int x = 3232;
-
-    public static void set_xor1(int x){
-        xor1 = x;
-    }
-
-    public void get_xor1(){
-        System.out.println(xor1);
-        set_xor1(12);
-        System.out.println(xor1);
-    }
-
-    public void test_peremennix(int x){
-
-        System.out.println(this.x);
+    public static void risks(int x) throws RuntimeException{
+        if(x == 0) throw  new RuntimeException();
+        else{
+            System.out.println("GG");
+        }
     }
 }
-
 
 public class main {
 
     public static void main(String[] args) {
-        /*
-        for(int x = 0; x < 5; x++){
-            System.out.println(new Test_Static().y);
-        }
-         */
 
-        Test_Static_UnStatic  first1 = new Test_Static_UnStatic();
-        first1.get_xor1();
-        first1.test_peremennix(333);
+
+        try{
+            test1.risks(0);
+        }
+        catch(RuntimeException e){
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
+        }
+        finally {
+            System.out.println("BB");
+        }
+
     }
 }
