b52995ef70531d1e1da9d04b73e71e38dc5d437c
watersmark <DuldinEV@mail.ru>
2020-09-06 12:36:44 +0300

    Ternarniy operator

---
+++ Ternarniy operator/src/Ternarniy.java
@@ -0,0 +1,12 @@
+public class Ternarniy {
+    public static void main(String[] args) {
+        int i = 10;
+
+        int k = i > 0 ? i : -i;
+        System.out.println(k);
+
+        i = -10;
+        k = i > 0 ? i: -i;
+        System.out.println(k);
+    }
+}
