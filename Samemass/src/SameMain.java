b3b24df1ee8657d44c6d27fb983bfaef66e154df
watersmark <DuldinEV@mail.ru>
2020-09-17 16:36:46 +0300

    SecondProblem

--- Samemass/src/SameMain.java
+++ Samemass/src/SameMain.java
@@ -2,53 +2,25 @@ class AreSame {
 
     public static boolean comp(int[] a, int[] b) {
 
-        if (a == null || b == null) {
-            return false;
-        }
+        if(a == null || b == null) return false;
 
-        else if (a.length == 0 && b.length == 0) {
-            return true;
-        }
 
-        else if (a.length == 0 && b.length != 0) {
-            return false;
-        }
-        else if(a.length != 0 && b.length == 0){
-            return false;
-        }
-
-        else if(a.length != 0 && b.length != 0){
-            int[] new_mass = new int[a.length];
-
-            for (int i = 0; i < a.length; i++) {
-                new_mass[i] = a[i] * a[i];
-            }
-            boolean bool = false;
-            for (int i = 0; i < b.length; i++) {
-
-                bool = false;
-
-                for (int j = 0; j < new_mass.length; j++) {
-
-                    if (b[i] == new_mass[j]) {
-                        bool = true;
-                        break;
-                    }
-
+        boolean bool = false;
+        for(int i : b){
+            for(int j : a){
+                if(i == (j * j)){
+                    bool = true;
+                    break;
                 }
-
-                if (!bool) return false;
             }
 
-            if(!bool) {
-                return false;
-            }
+            if(!bool) return false;
 
-            return true;
+            bool = false;
         }
-        else {
-            return false;
-        }
+
+        return true;
+
     }
 }
 
